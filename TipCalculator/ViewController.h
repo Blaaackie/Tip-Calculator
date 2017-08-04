8ec483d381457ca3628c3e072ae3de8e56e2e71f
Tye Blackie <blackie.tye@gmail.com>
2017-08-04 12:10:02 -0700

    Initial Commit

---
+++ TipCalculator/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TipCalculator
+//
+//  Created by Tye Blackie on 2017-08-04.
+//  Copyright © 2017 Tye Blackie. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
