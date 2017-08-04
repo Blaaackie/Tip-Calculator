379823be21b157ab196477e893220916f98f752e
Tye Blackie <blackie.tye@gmail.com>
2017-08-04 14:43:26 -0700

    initial Commit

--- TipCalculator/ViewController.m
+++ TipCalculator/ViewController.m
@@ -10,6 +10,11 @@
 
 @interface ViewController ()
 
+@property (weak, nonatomic) IBOutlet UITextField *billAmountTextField;
+@property (weak, nonatomic) IBOutlet UIButton *calculateTip;
+@property (weak, nonatomic) IBOutlet UITextField *tipPercantage;
+@property (weak, nonatomic) IBOutlet UILabel *tipAmount;
+
 @end
 
 @implementation ViewController
@@ -23,6 +28,11 @@
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+- (IBAction)calculateTip:(id)sender {
+    float billAmount = [self.billAmountTextField.text floatValue];
+    float tipAmount = billAmount * [[NSString stringWithFormat:@".%@", self.tipPercantage.text] floatValue];
+    self.tipAmount.text = [NSString stringWithFormat:@"%.2f", tipAmount];
 }
 
 
